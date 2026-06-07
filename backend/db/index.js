fc675a5c43ec40307da15fd4f1aca4b3bd7cfac9
SahilKumar78 <sahilabcd661@gmail.com>
2026-06-07 12:13:15 +0530

    database setup done

---
+++ backend/db/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+        
+        
+     try {
+
+
+           console.log("mongodb string is: " , process.env.MONGODB_URI);
+           
+           const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+         
+     } catch (error) {
+          console.log("MongoDb connection failed: ",error);
+          process.exit(1);
+          
+     }    
+}
+
+export  {connectDB};
--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,28 @@
-import express from "express"
-const app = router();
+import dotenv from "dotenv"
+
+
+import { app} from "./app.js"
+import {connectDB} from "./db/index.js"
+
+dotenv.config({
+    path:'./.env',
+    quiet: true,
+
+})
+
+
+app.get("/", (req, res) => {
+        res.send("<h1>Your student managament database is ready now</h1>")
+})
+
+connectDB()
+.then(() => {
+     app.listen(process.env.PORT || 8080, () => {
+           console.log(`server is running at port: ${process.env.PORT}`);
+           
+     })
+})
+.catch((error) => {
+      console.log("mongoDB connection failed!!", error);
+      
+})
